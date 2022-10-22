d2a887e87ab309daa27e49aa6895d837f736e26e
Brian O'Meara <omeara.brian@gmail.com>
2022-10-22 02:26:51 -0400

    more processing from sqlite

--- run.R
+++ run.R
@@ -1,13 +1,18 @@
+rm(list=ls())
 Sys.setenv(RSTUDIO_PANDOC="/usr/local/bin/pandoc")
 system("export PATH=$PATH:/usr/local/bin")
-
+Sys.setenv(R_MAX_VSIZE = 16e10)
 library(targets)
 source("_packages.R")
 source("R/functions.R")
 
+
+tar_option_set(memory = "transient", garbage_collection = TRUE)
+
+
 # Only render site if needing to do new pages
 
-rmarkdown::render_site()
+#rmarkdown::render_site()
 
 tar_make()
 
