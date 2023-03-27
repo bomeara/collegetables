eb31702c413778f15bd72ef91d733fd21e0c1368
Brian O'Meara <omeara.brian@gmail.com>
2023-03-27 16:29:30 -0400

    weekend work

--- _targets.R
+++ _targets.R
@@ -16,11 +16,12 @@ options(timeout=24*60*60) # let things download for at least 24 hours (important
 options(download.file.method = "libcurl")
 
 #tar_invalidate(pages)
-#tar_invalidate(index_et_al)
+tar_invalidate(index_et_al)
 #tar_invalidate(about)
 #tar_invalidate(sparklines)
 #tar_invalidate(state_pages)
 #tar_invalidate(pages_new)
+#tar_invalidate(comparison_table)
 
 # End this file with a list of target objects.
 list(
@@ -28,11 +29,11 @@ list(
  tar_target(ipeds_directly, GetIPEDSDirectly()),
  tar_target(ipeds_direct_and_db, AggregateDirectIPEDSDirect(ipeds_directly)),
  tar_target(comparison_table_core, CreateComparisonTables(ipeds_direct_and_db)),
- tar_target(comparison_table, AppendBiome(AppendAAUPCensure(AppendAbortion(AppendVaccination(AppendCATravelBan(AppendGunLaws(AppendMarriageRespect(AppendContraceptiveSupport(comparison_table_core))))))))),
+ tar_target(comparison_table, ReorderComparisonTable(AppendTransRisk(AppendBiome(AppendAAUPCensure(AppendAbortion(AppendVaccination(AppendCATravelBan(AppendGunLaws(AppendMarriageRespect(AppendContraceptiveSupport(comparison_table_core))))))))))),
  tar_target(index_table, CreateIndexTable(comparison_table)),
  tar_target(weatherspark, GetWeathersparkLinks()),
  tar_target(fields_and_majors, GetFieldsAndMajors(comparison_table, ipeds_direct_and_db, CIPS_codes)),
- tar_target(pages_new, RenderInstitutionPagesNew(comparison_table, fields_and_majors, maxcount=maxcount, CIPS_codes=CIPS_codes, weatherspark=weatherspark, yml=yml)),
+ tar_target(pages_new, RenderInstitutionPagesNew(comparison_table, fields_and_majors, maxcount=maxcount, CIPS_codes=CIPS_codes, weatherspark=weatherspark, yml=yml, index_table=index_table)),
  tar_target(CIPS_codes, GetCIPCodesExplanations()),
  tar_target(yml, CreateYMLForSite(CIPS_codes)),
  tar_target(field_pages, RenderFieldPages(CIPS_codes, fields_and_majors, yml)),
